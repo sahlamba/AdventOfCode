8728ca52843c57ad6ce411e9e6624ba2e1aa78e2
Sahil Lamba <sahil.lamba95@gmail.com>
2021-12-03 20:11:18 +0530

    2021/Day 2

---
+++ 2021/day2/part1.js
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import readline from 'readline'
+
+class Solution {
+  static x = 0
+  static depth = 0
+
+  static parseInput(input) {
+    const [dir, mag] = input.split(' ')
+    return [dir, Number.parseInt(mag)]
+  }
+
+  static move([dir, mag]) {
+    switch (dir) {
+      case 'forward':
+        this.x += mag
+        break
+      case 'down':
+        this.depth += mag
+        break
+      case 'up':
+        this.depth -= mag
+        break
+    }
+  }
+
+  static result() {
+    return this.x * this.depth
+  }
+}
+
+async function solve() {
+  const FILE = '/2021/day2/input.txt'
+
+  const fileStream = fs.createReadStream(process.cwd() + FILE)
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  })
+
+  for await (const line of rl) {
+    Solution.move(Solution.parseInput(line))
+  }
+
+  console.info(Solution.result())
+}
+
+solve()
